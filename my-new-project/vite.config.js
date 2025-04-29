0c76cf78658f8020fa86a6cf55a8bf894c1944d2
Амина Казанбасова <aminakazanbasova@MacBook-Air-Amina.local>
2025-04-29 13:58:54 +0500

    init

--- my-new-project/vite.config.js
+++ my-new-project/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/aminaa/"
   server: {
     assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg'],
     port: 5181,
